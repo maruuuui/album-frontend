1f300effb72449db98a3465364c3d7dc07742060
Mitsu <Mitsu@example.com>
2020-03-15 23:32:03 +0900

    separate overlay

--- src/App.vue
+++ src/App.vue
@@ -45,54 +45,15 @@
       </v-container>
     </v-content>
     <!-- カードクリック時に開くオーバーレイのコンテンツ -->
-    <v-dialog :value="overlay" :dark="false">
-      <v-card>
-        <v-toolbar color="gray" dark>
-          <v-toolbar-title>画像詳細</v-toolbar-title>
-          <v-spacer></v-spacer>
-
-          <v-btn color="primary" fab @click="overlay = false">&times;</v-btn>
-        </v-toolbar>
-
-        <v-row class="d-flex justify-center">
-          <v-img
-            :src="overlay_item.image"
-            contain
-            :max-width="0.8*window_width"
-            :max-height="0.8*window_height"
-          ></v-img>
-        </v-row>
-
-        <!-- <v-card-text class="text--primary"> -->
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-title>画像ファイル名</v-list-item-title>
-            <v-list-item-subtitle>{{ overlay_item.image_name }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-title>備考</v-list-item-title>
-            <v-list-item-subtitle>{{ overlay_item.memo }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-title>更新日時</v-list-item-title>
-            <v-list-item-subtitle>{{ overlay_item.updated_at }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-title>登録日時</v-list-item-title>
-            <v-list-item-subtitle>{{ overlay_item.created_at }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-        <v-card-actions>
-          <v-btn color="red" @click="deleteImage(overlay_item_url)">削除</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-dialog :value="showImageDetailOverlay" :dark="false">
+      <image-detail-overlay
+        :window_width="window_width"
+        :window_height="window_height"
+        :overlay_item="overlay_item"
+        :overlay_item_url="overlay_item_url"
+        @deleteImage="deleteImage"
+        @closeOverlay="closeOverlay"
+      />
     </v-dialog>
   </v-app>
 </template>
@@ -100,18 +61,20 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import imageCard from "./components/imageCard";
+import imageDetailOverlay from "./components/imageDetailOverlay";
 
 export default {
   name: "App",
 
   components: {
-    "image-card": imageCard
+    "image-card": imageCard,
+    "image-detail-overlay": imageDetailOverlay
   },
 
   data: () => ({
     image_items: [], //APIで取得してくる画像の入れ物
     showUploadForm: false,
-    overlay: false, //オーバーレイの表示状態
+    showImageDetailOverlay: false, //オーバーレイの表示状態
     overlay_item: {}, //オーバーレイに表示する対象
     overlay_item_url: "", //オーバーレイに表示する対象の編集に使うURL
     api_url: "http://127.0.0.1:8000/api/v1/image/",
@@ -141,9 +104,13 @@ export default {
     },
     //オーバーレイに表示する対象(レコード)の情報を入れ込んで表示
     showOverlay(image_item) {
-      this.overlay = !this.overlay;
+      this.showImageDetailOverlay = !this.showImageDetailOverlay;
       this.overlay_item = image_item;
       this.overlay_item_url = this.api_url + image_item.id + "/";
+    },
+    //オーバーレイを閉じる
+    closeOverlay() {
+      this.showImageDetailOverlay = false;
     },
     getImage(url) {
       this.image_items = [];
@@ -196,7 +163,7 @@ export default {
         .delete(url, requestHeaderWithNullData)
         .then(function() {
           alert("削除しました");
-          self.overlay = false;
+          self.showImageDetailOverlay = false;
           self.getImage();
         })
         .catch(function(error) {
---
+++ src/components/imageDetailOverlay.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card>
+    <v-toolbar color="gray" dark>
+      <v-toolbar-title>画像詳細</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn color="primary" fab @click="$emit('closeOverlay')">&times;</v-btn>
+    </v-toolbar>
+
+    <v-row class="d-flex justify-center">
+      <v-img
+        :src="overlay_item.image"
+        contain
+        :max-width="0.8*window_width"
+        :max-height="0.8*window_height"
+      ></v-img>
+    </v-row>
+
+    <!-- <v-card-text class="text--primary"> -->
+    <v-list class="transparent">
+      <v-list-item>
+        <v-list-item-title>画像ファイル名</v-list-item-title>
+        <v-list-item-subtitle>{{ overlay_item.image_name }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+
+    <v-list class="transparent">
+      <v-list-item>
+        <v-list-item-title>備考</v-list-item-title>
+        <v-list-item-subtitle>{{ overlay_item.memo }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+    <v-list class="transparent">
+      <v-list-item>
+        <v-list-item-title>更新日時</v-list-item-title>
+        <v-list-item-subtitle>{{ overlay_item.updated_at }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+    <v-list class="transparent">
+      <v-list-item>
+        <v-list-item-title>登録日時</v-list-item-title>
+        <v-list-item-subtitle>{{ overlay_item.created_at }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+    <v-card-actions>
+      <v-btn color="red" @click="$emit('deleteImage',overlay_item_url)">削除</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "imageDetailOverlay",
+  props: ["overlay_item", "overlay_item_url", "window_width", "window_height"]
+};
+</script>
